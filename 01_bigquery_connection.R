c5eefa330d7f438930681938f3b525e189a12c55
aryag4067-eng <aryag4067@gmail.com>
2026-01-31 21:48:50 +0530

    Create 01_bigquery_connection.R

---
+++ 01_bigquery_connection.R
@@ -0,0 +1,20 @@
+library(bigrquery)
+library(DBI)
+
+# This pulls the hidden ID from your .Renviron file
+project_id <- Sys.getenv("BQ_PROJECT_ID")
+
+# 1. Establish the Cloud Connection
+con <- dbConnect(
+  bigrquery::bigquery(),
+  project = project_id,
+  dataset = "cyclistic_data",
+  billing = project_id
+)
+
+# 2. Pull a sample for our initial analysis
+# We use the corrected table name we found: combined_2025_full_year
+df_sample <- dbGetQuery(con, "SELECT * FROM `inner-legacy-485410-s8.cyclistic_data.combined_2025_full_year` LIMIT 100")
+
+# 3. View the data in RStudio's beautiful spreadsheet viewer
+View(df_sample)
